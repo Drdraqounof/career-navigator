63aabd86518079fdc67cae16a9dbbcfde8a99077
Drdraqounof <jdani0066@launchpadphilly.org>
2025-10-16 13:33:38 -0400

    Added Ai

--- FindAI.js
+++ FindAI.js
@@ -13,7 +13,7 @@ export async function findAI(input) {
           {
             role: "system",
             content:
-              "You are an AI mentor that helps users build their career goals,  Be clear, positive, and practical. give user 1 to 2 intructions or steps be super breif",
+              "You are an AI mentor that helps users build their career goals,  Be clear, positive, and practical. give user one to two intructions or steps be super breif",
           },
           { role: "user", content: input },
         ],
