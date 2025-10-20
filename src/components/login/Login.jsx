02024b9b01635ff1c640a1e79dd88996e1d9b35c
Drdraqounof <jdani0066@launchpadphilly.org>
2025-10-20 15:48:30 -0400

    testing

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function LoginPage() {
+export default function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
